66bb4d6a6b1c97fea050bd004d327ba1eb12718c
solankishagun05-max <solankishagun05@gmail.com>
2026-05-28 23:50:56 -0700

    Add files via upload

--- dcl.sql
+++ dcl.sql
@@ -1,41 +1,33 @@
-CREATE database rajdb;
-use rajdb;
-create table raj45( id int);
-insert into raj45 values (10);
+use testdb4;
 
--- TCL 
-start transaction;
-insert into raj45 values (100),(90);
-select* from raj45;
-commit;
--- data ko chnage kar deta h or parmenantly save kr deta hai deta ko / consistint state m bhej deta h deta ko ......alter
-
-
--- commit /rollback /ddl statement 
-use rajdb;
-
-start transaction;
-update raj45 set id=500;
+grant select on testdb4.emp to ishan; -- for giving access 
+grant delete on testdb4.* to ishan ;
 set sql_safe_updates=0;
-select * from raj45;
+revoke delete on testdb4.emp from ishan;
+
+-- revoke this delete prevlige from the ishan user 
+-- give select and insert privilage to a particular table 
+-- create a table with three columns and insert atleast two rows 
+-- now give onw column acces to this select statement also give the update privilage to the second or third column  
+-- remove all the privilages from this ishan user 
 
 
-start transaction;
-insert into raj45 values (10),(11),(50),(68);
-delete from raj45 where id=10;
-select * from raj45;
-commit;
-rollback;
+create database sh;
+use sh;
+create table emp1( id int,name varchar (20),location varchar (10));
+insert into emp1 values ( 1,'nirbhay','jaipur'),(2,'mihir','udaipur');
+grant select(id) on sh.emp1 to ishan;
+grant update(name , location ) on sh.emp1 to ishan;
+revoke select on sh.emp1 from ishan;
+revoke update on sh.emp1 from ishan;
 
 
-start transaction;
-insert into raj45 values (768),(500),(400);
-savepoint raj45_savepoint_ins;
-update raj45 set id =1000;
-rollback to raj45_savepoint_ins;
+-- create three users and create a group (roll) with the name sales select,insert ,update and add user in the roll 
+-- login in roll and check permision granted or not 
 
--- drop and truncate are ddl statement  rollback nhi kar skte hai 
--- dlete is dml stetament rollback ho skta hai 
-
-
+create user a identified by '123';
+create user b identified by '123';
+create user c identified by '123';
+create role 'group' ;
+grant 'group' to 'a','b','c';  
 
